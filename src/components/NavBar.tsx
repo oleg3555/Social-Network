ec9f7c2f09641b38a24a3dd8718faddb8b16602c
oleg3555 <olegyanusik4@gmail.com>
2021-11-18 22:31:36 +0300

    start commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+export const NavBar = () => {
+    return (
+        <nav className="nav">
+            <div>Profile</div>
+            <div>Messages</div>
+            <div>News</div>
+            <div>Music</div>
+            <div>Settings</div>
+        </nav>
+    )
+}
